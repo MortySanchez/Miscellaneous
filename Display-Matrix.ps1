41843d634a82cc4de773d78263d380e023daba4e
MortySanchez <tobias.knobloch@steinlaender.de>
2022-05-30 00:41:13 +0200

    New function Display-Matrix to learn programming matrices

---
+++ Display-Matrix.ps1
@@ -0,0 +1,33 @@
+﻿# Learning to use matrices
+
+[System.Array]$numbers = @();
+for([int]$i = 0; $i -lt 25; $i++) {
+    $numbers += $i;
+}
+
+
+Function Display-Matrix() {
+    Param(
+        
+        [Parameter()]
+        [int]$Rows,
+
+        [Parameter()]
+        [int]$Columns,
+
+        [Parameter()]
+        [System.Array]$InputObject
+    )
+
+    [int]$index = 0;
+
+    for([int]$i = 0; $i -lt $Rows; $i++) {
+        for([int]$j = 0; $j -lt $Columns; $j++) {
+            Write-Host "$InputObject[$index]" -NoNewline;
+            $index++;
+        }
+        Write-Host "";
+    }
+}
+
+Display-Matrix -Rows 5 -Columns 5;
